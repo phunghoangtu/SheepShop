f45e900c9e2055da73c74c5ecaae5b6e26e879ed
phunghoangtu <tuphph311200@gmail.com>
2024-03-10 16:39:46 +0700

    bug

--- src/main/java/com/example/sheepshop/config/Encdode.java
+++
@@ -1,9 +0,0 @@
-package com.example.sheepshop.config;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-public class Encdode {
-    public static void main(String[] args) {
-        System.out.println(new BCryptPasswordEncoder().encode("123456"));
-    }
-}
--- src/main/java/com/example/sheepshop/services/impl/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.sheepshop.services.impl;
-
-import com.example.sheepshop.models.Users;
-
-public interface UserService {
-
-    Users findByUsername(String username);
-
-}
